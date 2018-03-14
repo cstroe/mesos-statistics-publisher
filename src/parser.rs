bd77cb31e096dcbf22666cd2b8a721f5e79e33d3
Cosmin Stroe <cosmin@mailbox.org>
2018-03-13 21:15:51 -0500

    Parse the slaves field.

--- src/parser.rs
+++ src/parser.rs
@@ -2,21 +2,28 @@ use serde_json::Error;
 use serde_json::from_str;
 
 #[derive(Serialize, Deserialize)]
+pub struct SlaveDto {
+    pub id: String,
+    pub hostname: String,
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct TaskDto {
-  pub id: String,
-  pub slave_id: String,
+    pub id: String,
+    pub slave_id: String,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct FrameworkDto {
-  pub name: String,
-  pub tasks: Vec<TaskDto>,
+    pub name: String,
+    pub tasks: Vec<TaskDto>,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct MesosStateDto {
-  pub id: String,
-  pub frameworks: Vec<FrameworkDto>,
+    pub id: String,
+    pub slaves: Vec<SlaveDto>,
+    pub frameworks: Vec<FrameworkDto>,
 }
 
 pub fn parse(json: String) -> Result<MesosStateDto, Error> {
--- tests/parser_test.rs
+++ tests/parser_test.rs
@@ -27,6 +27,13 @@ fn it_parses_json() {
     let marathon = &frameworks[0];
     assert_eq!("marathon", marathon.name);
     assert_eq!(0, marathon.tasks.len());
+
+    let slaves = state.slaves;
+    assert_eq!(1, slaves.len());
+
+    let slave = &slaves[0];
+    assert_eq!("e456dbcf-b4e2-4506-9040-21a1669152db-S0", slave.id);
+    assert_eq!("172.17.0.6", slave.hostname);
 }
 
 #[test]
@@ -41,4 +48,11 @@ fn it_parses_tasks() {
     assert_eq!(1, marathon.tasks.len());
     assert_eq!("postgres.6c0ad1b7-26be-11e8-a7a4-0242ac110006", marathon.tasks[0].id);
     assert_eq!("26c5dc7e-1450-41d6-8b86-56b9af06fdc2-S0", marathon.tasks[0].slave_id);
+
+    let slaves = state.slaves;
+    assert_eq!(1, slaves.len());
+
+    let slave = &slaves[0];
+    assert_eq!("26c5dc7e-1450-41d6-8b86-56b9af06fdc2-S0", slave.id);
+    assert_eq!("172.17.0.7", slave.hostname);
 }
